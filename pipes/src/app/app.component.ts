2eecc23cf1f71210b524f551335228d56a38f7b6
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-11-05 01:01:14 -0500

    Listo los pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -13,7 +13,9 @@ export class AppComponent {
   porcentaje: number = 0.234;
   salario: number = 1234.5;
   fecha  : Date = new Date();
+  activar : boolean = true;
   idioma : string = 'es';
+  videUrl: string = 'https://www.youtube.com/embed/Q_gIYp078so';
 
   valorPromesa = new Promise<string>( (resolve) => {
     setTimeout(() => {
