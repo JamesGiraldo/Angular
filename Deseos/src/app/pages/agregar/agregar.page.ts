8a94b6c2bd16ca3f23b23aa2074e78e13a2ba408
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-11-20 02:41:29 -0500

    Cada ves mejor cabron

---
+++ Deseos/src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Lista } from '../../models/lista.model';
+
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage implements OnInit {
+
+  lista: Lista;
+
+  constructor( private deseosService: DeseosService,
+               private route: ActivatedRoute) {
+
+    const listaId = this.route.snapshot.paramMap.get('listaId');
+    this.lista = this.deseosService.obtenerLista(listaId);
+    console.log(this.lista);
+  }
+
+  ngOnInit() {
+  }
+
+}
